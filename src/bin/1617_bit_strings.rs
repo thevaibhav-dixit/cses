d75d36e0fda0c4bff291e7150a0d7c62fbd1592f
Vaibhav <thevaibhavdixit@gmail.com>
2026-05-31 17:16:59 +0530

    feat: bit_strings + trailing_zeros

--- src/bin/1617_bit_strings.rs
+++ src/bin/1617_bit_strings.rs
@@ -2,5 +2,14 @@
 // Category: Introductory Problems
 // https://cses.fi/problemset/task/1617
 
+use std::io::Write;
+
 fn main() {
+    let mut sc = cses::stdin();
+    let mut out = cses::stdout();
+
+    const MOD: u64 = 1_000_000_007;
+    let n: i64 = sc.next();
+    let ans = (1..=n).fold(1, |acc, _| (acc * 2) % MOD);
+    writeln!(out, "{}", ans).unwrap();
 }
--- src/bin/1618_trailing_zeros.rs
+++ src/bin/1618_trailing_zeros.rs
@@ -2,5 +2,19 @@
 // Category: Introductory Problems
 // https://cses.fi/problemset/task/1618
 
+use std::io::Write;
+
 fn main() {
+    let mut sc = cses::stdin();
+    let mut out = cses::stdout();
+
+    let n: i64 = sc.next();
+    // legendres formula: n! has n/5 + n/25 + n/125 + ... trailing zeros
+    let mut curr_power_of_5 = 5;
+    let mut ans = 0;
+    while curr_power_of_5 <= n {
+        ans += n / curr_power_of_5;
+        curr_power_of_5 *= 5;
+    }
+    writeln!(out, "{}", ans).unwrap();
 }
