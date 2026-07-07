634da6912cc3e3918719b6fc1c97307e00d32a21
Vaibhav <thevaibhavdixit@gmail.com>
2026-07-07 20:12:14 +0530

    feat: gray_code

--- src/bin/2205_gray_code.rs
+++ src/bin/2205_gray_code.rs
@@ -2,5 +2,17 @@
 // Category: Introductory Problems
 // https://cses.fi/problemset/task/2205
 
+use std::io::Write;
+
 fn main() {
+    let mut sc = cses::stdin();
+    let mut out = cses::stdout();
+
+    let n: u32 = sc.next();
+    let size = 1 << n;
+
+    for i in 0..size {
+        let gray_code = i ^ (i >> 1);
+        writeln!(out, "{:0width$b}", gray_code, width = n as usize).unwrap();
+    }
 }
