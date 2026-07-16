70f3b02f820c98c0e125c425401168b0cd72b860
Vaibhav <thevaibhavdixit@gmail.com>
2026-07-16 19:50:54 +0530

    feat: mex_grid

--- src/bin/3419_mex_grid_construction.rs
+++ src/bin/3419_mex_grid_construction.rs
@@ -2,5 +2,18 @@
 // Category: Introductory Problems
 // https://cses.fi/problemset/task/3419
 
+use std::io::Write;
+
 fn main() {
+    let mut sc = cses::stdin();
+    let mut out = cses::stdout();
+
+    let n: usize = sc.next();
+    for i in 0..n {
+        let row = (0..n)
+            .map(|j| (i ^ j).to_string())
+            .collect::<Vec<_>>()
+            .join(" ");
+        writeln!(out, "{}", row).unwrap();
+    }
 }
